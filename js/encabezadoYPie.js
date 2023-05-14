0bc4ce9356e2e32a6905a0eb5979314293dd6751
ailin-p <ailinparedes97@gmail.com>
2023-05-14 20:00:48 -0300

    mejorando footer

--- js/encabezadoYPie.js
+++ js/encabezadoYPie.js
@@ -32,11 +32,39 @@ let hdr = `<h1>Mascotienda</h1>
 
         `;
        
-let ftr = `<div id="social-icons">
-            <a href="#"><i class="fa-brands fa-instagram" style="color: #000000; font-size: 30px; height: 30px; width: 30px;"></i></a>
-            <a href="#"><i class="fa-brands fa-facebook" style="color: #000000; font-size: 30px; height: 30px; width: 30px;"></i></a>
-            </div>
-            <p>MASCOTIENDA</p>`;
+let ftr = `<footer>
+<div class="container">
+    <div class="col-4 col-m-6 col-s-12">
+      <h3>Contacto</h3>
+      <ul id="info">
+        <li><i class="fa-solid fa-location-dot" style="color: #000000; font-size: 1.5em"></i>Alguna dirección 1570, Algún lugar, Argentina</li>
+        <li><i class="fa-brands fa-whatsapp" style="color: #000000; font-size: 1.5em"></i>555-444-333 </li>
+        <li><i class="fa-solid fa-envelope" style="color: #000000; font-size: 1.5em"></i>info@mascotienda.com</li>
+      </ul>
+    </div>
+    <div class="col-4 col-m-6 col-s-12">
+      <h3>Síguenos</h3>
+      <ul class="social-media">
+      <li><a href="#"><i class="fa-brands fa-facebook" style="color: #000000; font-size: 1.5em"></i></a></li>
+        <li><a href="#"><i class="fa-brands fa-twitter" style="color: #000000; font-size: 1.5em"></i></a></li>
+        <li><a href="#"><i class="fa-brands fa-instagram" style="color: #000000; font-size: 1.5em"></i></a></li>
+      </ul>
+    </div>
+    <div class="col-4 col-m-6 col-s-12">
+      <h3>Newsletter</h3>
+      <form>
+        <div class="form-group">
+          <input type="email" class="form-control" placeholder="Ingresa tu email">
+        </div>
+        <button type="submit" class="btn btn-primary">Suscríbete</button>
+      </form>
+    </div>
+  </div>
+<div class="bottom-footer">
+  <p>&copy; 2023 Mascotienda. Todos los derechos reservados.</p>
+</div>
+</footer>
+`;
         
 document.getElementsByTagName("header").item(0).innerHTML=hdr;
 document.getElementsByTagName("footer").item(0).innerHTML=ftr;
